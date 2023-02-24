0ae3dc0cdeebc17300d7b8628589ff7860ac1038
Tim Jedrek <tim@timjedrek.com>
2023-02-23 20:52:17 -0600

    start red section (added font)

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -11,6 +11,7 @@ module.exports = {
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        gloock: ['Gloock', ...defaultTheme.fontFamily.serif]
       },
       keyframes: {
         //mobile
